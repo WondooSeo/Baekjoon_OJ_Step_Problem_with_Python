7043aa942c9ad1d984e3e008bf7b49d6abaae34a
WDSeo <wdseo@yonsei.ac.kr>
2022-06-22 23:16:35 +0900

    분수찾기

--- 7. 기본 수학 1/Q1193.py
+++ 7. 기본 수학 1/Q1193.py
@@ -2,10 +2,15 @@ import sys
 
 if __name__ == '__main__':
     X = int(sys.stdin.readline().rstrip())
-    now_count, temp = 1, 0
+    now_count, total_count = 1, 0
     while True:
-        temp += now_count
-        if temp >= X:
+        total_count += now_count
+        if total_count >= X:
             break
         now_count += 1
 
+    bunja = total_count - X + 1
+    if now_count%2 == 1:
+        print(f'{bunja}/{now_count - bunja + 1}')
+    else:
+        print(f'{now_count - bunja + 1}/{bunja}')
