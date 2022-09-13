94e866283d17d8c397095b18708f3b512ec572d5
Won-Doo Seo <miracleboy96@naver.com>
2022-09-13 18:41:24 +0900

    좌표 정렬하기

---
+++ 9. 정렬/Q11650.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == '__main__':
+    coord = list()
+    for N in range(int(sys.stdin.readline().rstrip())):
+        coord.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+    coord.sort(key=lambda x:(x[0], x[1]))
+    for now_coord in coord:
+        print(*now_coord)
